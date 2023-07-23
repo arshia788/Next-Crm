22459fff976fa4bef3f9b83450d99ede6dbe8492
Amirali Amanipour <arshia.ala80@gmail.com>
2023-07-23 19:45:13 +0330

    form save&delete functions

--- components/templates/AddCustomerPage.js
+++ components/templates/AddCustomerPage.js
@@ -1,26 +1,61 @@
 import React, { useState } from 'react'
 import Form from '../modules/Form'
+import { useRouter } from 'next/router'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function AddCustomerPage() {
 
-    const [form, setForm]= useState({
-        name:'', 
-        email:'',
-        lastName:'',
-        phone:'',
-        address:'',
-        postalCode:'',
-        date:'',
-        products:[],
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        lastName: '',
+        phone: '',
+        address: '',
+        postalCode: '',
+        date: '',
+        products: [],
     })
+    const router = useRouter();
 
-    const cancelHandler=()=>{
+    const saveHandler = async () => {
+        const res = await fetch('/api/customer', {
+            method: "POST",
 
+            // !havaset bashe ke form ro be name data dadi be back
+
+            body: JSON.stringify({ data: form }),
+            headers: { "Content-Type": "application/json" }
+        })
+        const data = await res.json();
+
+        if (data.status === "success") {
+            toast.success("The user created", {
+                position:"top-right",
+                closeOnClick:true,
+                theme:"colored"
+            })
+            router.push('/');
+        }
     }
 
-    const saveHandler=()=>{
-        
+    const cancelHandler = async () => {
+        setForm({
+            name: '',
+            email: '',
+            lastName: '',
+            phone: '',
+            address: '',
+            postalCode: '',
+            date: '',
+            products: [],
+        }),
+
+        router.push('/')
     }
+
+
 
     return (
         <div className='flex flex-col'>
@@ -29,14 +64,17 @@ export default function AddCustomerPage() {
 
             <div className='flex justify-between items-center'>
                 <button
-                className='bg-red-500 rounded px-3 py-1'
-                onClick={cancelHandler}
+                    className='bg-red-500 rounded px-3 py-1'
+                    onClick={cancelHandler}
                 >cancel</button>
                 <button
-                onClick={saveHandler}
-                className='bg-green-500 rounded px-3 py-1 '
+                    onClick={saveHandler}
+                    className='bg-green-500 rounded px-3 py-1 '
                 >save</button>
             </div>
+
+            <ToastContainer />
+
         </div>
     )
 }
